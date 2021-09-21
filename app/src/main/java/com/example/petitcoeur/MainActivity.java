9d210c9c0a8b8a82f557a618f3cec5456e0c588a
Benoit Guilhaumon <benoit1399@gmail.com>
2021-09-21 17:59:33 +0200

    All project

--- app/src/main/java/com/example/petitcoeur/MainActivity.java
+++ app/src/main/java/com/example/petitcoeur/MainActivity.java
@@ -1,38 +1,58 @@
 package com.example.petitcoeur;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.DialogInterface;
 import android.content.DialogInterface.OnClickListener;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends Activity {
+
+    public static final String TAG = "UPCoeur";
+    private EditText editName;
+
+    @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        editName = findViewById((R.id.name));
+
+        // On restaure suite à un OnCreate
+        if(savedInstanceState != null && savedInstanceState.containsKey("personname")){
+            String personname = savedInstanceState.getString("personname");
+            editName.setText(personname);
+        }
     }
 
-    public void action_start(Bundle savedInstanceState) {
-        Button button_start = null;
-
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        button_start = (Button) findViewById(R.id.start);
-
-        button_start.setOnClickListener(new View.OnClickListener() {
-
-            public void onClick(View button11) {
-
-                Intent intent1 = new Intent(MainActivity.this, je_suis.class);
-
-                startActivity(intent1);
-            }
-
-        });
+    // Permet de passer à la premiere page du formulaire via le bouton start
+    public void action_start(View sender){
+        Log.d(TAG, "action_start: Demarrage du test");
+        Intent intent = new Intent(this, Profil.class);
+        intent.putExtra("personname",editName.getText().toString());// Permet de transferer la donnée de nom lors du changement de page
+        startActivity(intent);
     }
 
+    @Override // C'est android qui appel cette fonction de sauvegarde
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putString("personname", editName.getText().toString()); // On sauvegarde le nom du pokémon
+    }
+
+    @Override // C'est android qui appel cette fonction de restauration
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        if (savedInstanceState.containsKey("personname")) {// On verifie qu'il y ait bien un resultat
+            String personname = savedInstanceState.getString("personname");
+            editName.setText(personname);
+        }
+    }
 }
--- app/src/main/java/com/example/petitcoeur/Profil.java
+++ app/src/main/java/com/example/petitcoeur/Profil.java
@@ -2,13 +2,36 @@ package com.example.petitcoeur;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 
 public class Profil extends AppCompatActivity {
+
+    public static final String TAG = MainActivity.TAG;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profil);
+
+        Log.d(TAG, "onCreate: ");
+        // On recupere la valeur envoyée par la page précédente
+        Intent intent = getIntent();
+        String name = intent.getStringExtra("personname");
+        Log.d(TAG, "onCreate: Person name : " + name);
+    }
+
+    // Permet de passer à la premiere page du formulaire via le bouton start
+    public void action_next_page(View sender){
+        Log.d(TAG, "action_next_page: Passage à la suite du formulaire");
+        Intent intent = new Intent(this, MonCoeur.class);
+        startActivity(intent);
+    }
+
+    public void action_finish(View v){
+        finish(); //Permet de revenir à la page initiale
     }
 }
