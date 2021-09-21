84fe5b7dccac88686e28123d1b2ab2489d79b7cc
Benoit Guilhaumon <benoit1399@gmail.com>
2021-09-21 16:42:22 +0200

    All project

---
+++ app/src/main/java/com/example/petitcoeur/MonCoeur.java
@@ -0,0 +1,14 @@
+package com.example.petitcoeur;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MonCoeur extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mon_coeur);
+    }
+}
---
+++ app/src/main/java/com/example/petitcoeur/Suivi_Cardiaque.java
@@ -0,0 +1,14 @@
+package com.example.petitcoeur;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Suivi_Cardiaque extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_suivi_cardiaque);
+    }
+}
