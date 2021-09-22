b64236858fa51d92e5ffb7cade7d9473ab4e028f
Leana <leana.mousset@hotmail.fr>
2021-09-22 16:43:07 +0200

    Add hygiene_vie

---
+++ app/src/main/java/com/example/petitcoeur/hygiene_vie.java
@@ -0,0 +1,14 @@
+package com.example.petitcoeur;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class hygiene_vie extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hygiene_vie);
+    }
+}
