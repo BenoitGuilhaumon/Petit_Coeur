fe4ee2bcdb5efb1b1dacbca589be4ca3a54f93e9
Leana <leana.mousset@hotmail.fr>
2021-09-23 15:41:26 +0200

    Link to French Féderation Cardiology

--- app/src/main/java/com/example/petitcoeur/hygiene_vie.java
+++ app/src/main/java/com/example/petitcoeur/hygiene_vie.java
@@ -2,13 +2,38 @@ package com.example.petitcoeur;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 
 public class hygiene_vie extends AppCompatActivity {
+
+    public static final String TAG = MainActivity.TAG;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hygiene_vie);
+
+        Log.d(TAG, "onCreate: ");
+        // On recupere la valeur envoyée par la page précédente
+        Intent intent = getIntent();
+        String name = intent.getStringExtra("coeur");
+        Log.d(TAG, "onCreate: Person name : " + name);
     }
-}
+
+        public void on_Click () {
+            String url = "https://fedecardio.org";
+            Log.d(TAG, "onClick: URL: " + url);
+            Intent intent = new Intent(Intent.ACTION_VIEW);
+            intent.setData(Uri.parse(url));
+            startActivity(intent);
+        }
+
+    }
+
+
+
+
+
